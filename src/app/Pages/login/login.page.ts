be3215fb590342d0d48fa4b5f0f806740b350f8f
Victor_99 <vamonarrez21@gmail.com>
2025-12-08 10:55:34 -0600

    aaa

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -52,7 +52,7 @@ export class LoginPage implements OnInit, AfterViewInit {
   async login() {
     console.log('Click en login detectado');
     try {
-      const response = await fetch('https://143.110.205.116/api/auth/login', {
+      const response = await fetch('https://grupoduran.shop/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
