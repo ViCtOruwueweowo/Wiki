89456518526e3cd4adc0e6ba2f511c0b3b452631
Victor_99 <vamonarrez21@gmail.com>
2025-12-08 09:21:44 -0600

    aaa

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,18 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { isDevMode } from '@angular/core';
-import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-
-    // ✅ Registro de Service Worker de Angular
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),  // Solo activo en producción
-      registrationStrategy: 'registerWhenStable:30000' // 30s después de cargar
-    }),
   ],
 });
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', { scope: '/' })
+    .then((registration) => console.log('✅ SW registrado:', registration.scope))
+    .catch((error) => console.error('❌ Error al registrar SW:', error));
+}
